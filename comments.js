cb71a4726e5af4b0858a8e6cebdb9379656db8b2
onegitter <mnu202001@gmail.com>
2024-06-10 21:25:14 +0530

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,44 @@
 // create web server
-const express = require('express');
-const app = express();
-// import data
-const comments = require('./comments.json');
-// create a route
-app.get('/comments', (req, res) => {
-    res.send(comments);
-});
-// start server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var path = require('path');
+var https = require('https');
+var http = require('http');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Run the server
-// $ node comments.js
-// Server is running on http://localhost:3000
-// Open http://localhost:3000/comments in your browser or any REST client.
+app.get('/comments', function (req, res) {
+  fs.readFile(__dirname + '/comments.json', function (err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
+});
+
+app.post('/comments', function (req, res) {
+  fs.readFile(__dirname + '/comments.json', function (err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(__dirname + '/comments.json', JSON.stringify(comments, null, 4), function (err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
+});
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
+
+var httpServer = http.createServer(app);
+httpServer.listen(80);
+console.log('Server is running on https://localhost:443/ and http://localhost:80/');
